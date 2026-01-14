d040bbe0a51c907388eb8aed30aebbd6818d2a56
Akogahira <bbtrivino@gmail.com>
2026-01-14 17:11:47 +0100

    front-end actualizado

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -4,10 +4,12 @@ import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
 import Maps from "./pages/Maps";
+import MapsBack from "./pages/MapsBack";
 
 export default function App() {
   return (
     <BrowserRouter>
+<<<<<<< HEAD
       <div className="appShell">
         <Header />
 
@@ -20,6 +22,15 @@ export default function App() {
 
         <Footer />
       </div>
+=======
+      < Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/maps" element={<Maps />} />
+        <Route path="/maps-back" element={<MapsBack />} /> {/* prueba/equipo */}
+      </Routes>
+      < Footer />
+>>>>>>> 48b3bc1d54da60e2233aeb1f8c34196a16c5b867
     </BrowserRouter>
   );
 }
